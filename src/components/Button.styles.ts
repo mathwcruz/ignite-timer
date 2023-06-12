e5cd4070eacc7bdfdb2639ba9f7e7e1cc9b66287
Matheus da Cruz <matheuswachcruz@gmail.com>
2023-06-12 09:50:52 -0300

    first commit

---
+++ src/components/Button.styles.ts
@@ -0,0 +1,18 @@
+import styled, { css } from "styled-components";
+
+export type ButtonVariant = "primary" | "secondary" | "danger" | "success";
+
+interface ButtonContainerProps {
+  variant: ButtonVariant;
+}
+
+export const ButtonContainer = styled.button<ButtonContainerProps>`
+  width: 100px;
+  height: 40px;
+
+  ${(props) => {
+    return css`
+      background-color: ${props.theme[props.variant]};
+    `;
+  }}
+`;
