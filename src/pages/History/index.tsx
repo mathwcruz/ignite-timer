e159601b05903355c72eb6e811d1c0ad010f3b2d
Matheus da Cruz <matheuswachcruz@gmail.com>
2023-06-13 11:40:54 -0300

    feat: create StatusBadge component

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,7 +1,54 @@
+import { HistoryContainer, HistoryList, StatusBadge } from "./styles";
+
 export function History() {
   return (
-    <div>
-      History
-    </div>
+    <HistoryContainer>
+    <h1>Meu histórico</h1>
+
+    <HistoryList>
+      <table>
+        <thead>
+          <tr>
+            <th>Tarefa</th>
+            <th>Duração</th>
+            <th>Duração</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Tarefa 1</td>
+            <td>20 minutos</td>
+            <td>Há 2 meses</td>
+            <td>
+              <StatusBadge statusColor="green">
+                Concluído
+              </StatusBadge>
+            </td>
+          </tr>
+          <tr>
+            <td>Tarefa 2</td>
+            <td>20 minutos</td>
+            <td>Há 2 meses</td>
+            <td>
+              <StatusBadge statusColor="yellow">
+                Em andamento
+              </StatusBadge>
+            </td>
+          </tr>
+          <tr>
+            <td>Tarefa 3</td>
+            <td>20 minutos</td>
+            <td>Há 4 meses</td>
+            <td>
+              <StatusBadge statusColor="red">
+                Interrompido
+              </StatusBadge>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </HistoryList>
+  </HistoryContainer>
   )
 }
