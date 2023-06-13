3cafb1ca68050d79a5be1ba414903a441b118866
Matheus da Cruz <matheuswachcruz@gmail.com>
2023-06-13 09:43:09 -0300

    feat: add routes to app and upload ignite logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import { ThemeProvider } from "styled-components"
+import { ThemeProvider } from "styled-components";
+import { BrowserRouter } from "react-router-dom";
 
-import { defaultTheme } from "./styles/themes/default"
-import { GlobalStyle } from "./styles/global"
+import { Router } from "./Router";
+
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      Hello World
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
-  )
+  );
 }
