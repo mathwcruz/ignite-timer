87ffe8a6159009be6d9fa749a536ae35a5b0a29d
Matheus da Cruz <matheuswachcruz@gmail.com>
2023-06-22 09:17:50 -0300

    feat: mapping cycles on the table history

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,54 +1,55 @@
+import { useContext } from "react";
+import { formatDistanceToNow } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
+
+import { CyclesContext } from "../../contexts/CyclesContext";
+
 import { HistoryContainer, HistoryList, StatusBadge } from "./styles";
 
 export function History() {
+  const { cycles } = useContext(CyclesContext);
+
   return (
     <HistoryContainer>
-    <h1>Meu histórico</h1>
+      <h1>Meu histórico</h1>
 
-    <HistoryList>
-      <table>
-        <thead>
-          <tr>
-            <th>Tarefa</th>
-            <th>Duração</th>
-            <th>Duração</th>
-            <th>Status</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Tarefa 1</td>
-            <td>20 minutos</td>
-            <td>Há 2 meses</td>
-            <td>
-              <StatusBadge statusColor="green">
-                Concluído
-              </StatusBadge>
-            </td>
-          </tr>
-          <tr>
-            <td>Tarefa 2</td>
-            <td>20 minutos</td>
-            <td>Há 2 meses</td>
-            <td>
-              <StatusBadge statusColor="yellow">
-                Em andamento
-              </StatusBadge>
-            </td>
-          </tr>
-          <tr>
-            <td>Tarefa 3</td>
-            <td>20 minutos</td>
-            <td>Há 4 meses</td>
-            <td>
-              <StatusBadge statusColor="red">
-                Interrompido
-              </StatusBadge>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </HistoryList>
-  </HistoryContainer>
-  )
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Duração</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cycles?.map((cycle) => (
+              <tr key={cycle?.id}>
+                <td>{cycle?.task}</td>
+                <td>{cycle?.minutesAmount} minutos</td>
+                <td>
+                  {formatDistanceToNow(cycle?.startDate, {
+                    addSuffix: true,
+                    locale: ptBR,
+                  })}
+                </td>
+                <td>
+                  {!!cycle?.finishedDate && (
+                    <StatusBadge statusColor="green">Concluído</StatusBadge>
+                  )}
+                  {!!cycle?.interruptedDate && (
+                    <StatusBadge statusColor="red">Interrompido</StatusBadge>
+                  )}
+                  {!cycle?.finishedDate && !cycle?.interruptedDate && (
+                    <StatusBadge statusColor="yellow">Em andamento</StatusBadge>
+                  )}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  );
 }
