c5873cd01f8d8e5392e82e285d33cd5a2525c4c9
Matheus da Cruz <matheuswachcruz@gmail.com>
2023-06-24 15:17:37 -0300

    feat: use immer to deal with reducer operations

--- src/reducers/cycles-state/reducer.ts
+++ src/reducers/cycles-state/reducer.ts
@@ -1,3 +1,5 @@
+import { produce } from "immer";
+
 import { CyclesStateActionTypes } from "./actions";
 
 export interface Cycle {
@@ -17,36 +19,40 @@ interface CyclesState {
 export const cyclesStateReducer = (state: CyclesState, action: any) => {
   switch (action.type) {
     case CyclesStateActionTypes.ADD_NEW_CYCLE:
-      return {
-        ...state,
-        cycles: [...state.cycles, action.payload.newCycle],
-        activeCycleId: action.payload.newCycle.id,
-      };
+      return produce(state, (draft) => {
+        draft.cycles.push(action.payload.newCycle);
+        draft.activeCycleId = action.payload.newCycle.id;
+      });
 
-    case CyclesStateActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED:
-      return {
-        ...state,
-        cycles: state.cycles?.map((cycle) => {
-          if (cycle?.id === state.activeCycleId) {
-            return { ...cycle, finishedDate: new Date() };
-          } else {
-            return cycle;
-          }
-        }),
-      };
+    case CyclesStateActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED: {
+      const currentCycleIndex = state?.cycles?.findIndex(
+        (cycle) => cycle?.id === state.activeCycleId
+      );
 
-    case CyclesStateActionTypes.INTERRUPT_CURRENT_CYCLE:
-      return {
-        ...state,
-        cycles: state.cycles?.map((cycle) => {
-          if (cycle?.id === state.activeCycleId) {
-            return { ...cycle, interruptedDate: new Date() };
-          } else {
-            return cycle;
-          }
-        }),
-        activeCycleId: null,
-      };
+      if (currentCycleIndex < 0) {
+        return state;
+      }
+
+      return produce(state, (draft) => {
+        draft.activeCycleId = null;
+        draft.cycles[currentCycleIndex].finishedDate = new Date();
+      });
+    }
+
+    case CyclesStateActionTypes.INTERRUPT_CURRENT_CYCLE: {
+      const currentCycleIndex = state?.cycles?.findIndex(
+        (cycle) => cycle?.id === state.activeCycleId
+      );
+
+      if (currentCycleIndex < 0) {
+        return state;
+      }
+
+      return produce(state, (draft) => {
+        draft.activeCycleId = null;
+        draft.cycles[currentCycleIndex].interruptedDate = new Date();
+      });
+    }
 
     default:
       return state;
